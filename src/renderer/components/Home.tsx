470f9352e0a7faf6eb8fa879eab487a1af754b04
Gilles Coolen <gilleswhc@gmail.com>
2020-02-04 15:16:28 +0100

    Intial commit

---
+++ src/renderer/components/Home.tsx
@@ -0,0 +1,10 @@
+import { hot } from 'react-hot-loader/root';
+import * as React from 'react';
+
+const Application = () => (
+  <div>
+    Ohio Gozaimassu
+    </div>
+);
+
+export default hot(Application);
