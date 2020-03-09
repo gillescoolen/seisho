3fc9332568c2dfb47b14fbcdde2b404939d59899
Gilles Coolen <gilleswhc@gmail.com>
2020-03-09 21:13:34 +0100

    Add hover effect to covers

--- src/renderer/components/Manga/Cover.tsx
+++ src/renderer/components/Manga/Cover.tsx
@@ -1,27 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from "react-router-dom";
 import { hot } from 'react-hot-loader/root';
 import { Manga } from '../../../domain/manga';
-import { Link } from "react-router-dom";
 
 const Cover = (props: { manga: Manga }) => {
   return (
-    <Link to={{ pathname: props.manga.getDetailsLink(), state: props.manga }}>
-      <StyledManga manga={props.manga}>
+    <StyledLink to={{ pathname: props.manga.getDetailsLink(), state: props.manga }}>
+      <StyledCover manga={props.manga}>
         <Title>{props.manga.getTitle()}</Title>
-      </StyledManga>
-    </Link>
+      </StyledCover>
+    </StyledLink>
   )
 };
 
-const StyledManga = styled.div`
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`;
+
+const StyledCover = styled.div`
   color: white;
   width: 250px;
   height: 400px;
   display: flex;
   background-size: cover;
   flex-direction: column-reverse;
+  transition: 0.5s all;
   background-image: linear-gradient(0deg, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0) 50%), ${(props: { manga: Manga }) => `url(${props.manga.getThumbnailUrl()})`};
+
+  &:hover {
+    cursor: pointer;
+    transition: 0.1s all;
+    box-shadow: 0px 0px 0px 5px #ee4540;
+  }
+
+  a {
+    color: white;
+    text-decoration: none;
+  }
 `;
 
 const Title = styled.h2`
