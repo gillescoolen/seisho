d381f5c3a1844687358088526ceaea7d49d81ab9
Gilles Coolen <gilleswhc@gmail.com>
2020-03-09 21:13:11 +0100

    Fix chapter order

--- src/renderer/components/Manga/ChapterList.tsx
+++ src/renderer/components/Manga/ChapterList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import { Manga } from '../../../domain/manga';
 import Spinner from '../UI/Spinner';
+import styled from 'styled-components';
 
 const ChapterList = (props: { manga: Manga }) => {
   if (props.manga.getChapters().length === 0) {
@@ -12,10 +13,21 @@ const ChapterList = (props: { manga: Manga }) => {
     )
   }
   return (
-    <ul>
-      {props.manga.getChapters().map((chapter, index) => <li key={index}>{chapter.getTitle()}</li>)}
-    </ul>
+    <List>
+      {props.manga.getChapters().reverse().map((chapter, index) => <li key={index}>{chapter.getTitle()}</li>)}
+    </List>
   )
 };
+
+const List = styled.ul`
+  padding: 0;
+  list-style: none;
+  border: 3px solid blue;
+
+  li {
+    padding: 0.5rem;
+    border: 1px solid green;
+  }
+`;
 
 export default hot(ChapterList);
--- src/renderer/components/Manga/Single.tsx
+++ src/renderer/components/Manga/Single.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { hot } from 'react-hot-loader/root';
 import { Manga } from '../../../domain/manga';
 import { ChapterList } from '.';
+import styled from 'styled-components';
 
 const Single = (props: any) => {
   const [manga] = useState<Manga>(props.location.state);
@@ -9,17 +10,39 @@ const Single = (props: any) => {
 
   useEffect(() => {
     (async () => {
+      // window.scrollTo(0, 0);
       await manga.fetchDetails();
       setLoading(false);
     })();
   }, [loading]);
   return (
-    <div>
-      <h1>{manga.getTitle()}</h1>
-      <p>{manga.getDescription()}</p>
-      <ChapterList manga={manga} />
-    </div>
+    <Container>
+      <Info>
+        <img src={manga.getThumbnailUrl()}/>
+        <h1>{manga.getTitle()}</h1>
+        <p>{manga.getDescription()}</p>
+      </Info>
+      <Chapters>
+        <ChapterList manga={manga} />
+      </Chapters>
+    </Container>
   )
 };
+
+const Container = styled.div`
+  padding: 1rem;
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-template-areas: "info chapters" "widgets chapters" "widgets chapters";
+`;
+
+const Info = styled.div`
+  grid-area: info;
+`;
+
+const Chapters = styled.div`
+  grid-area: chapters;
+`;
 
 export default hot(Single);
