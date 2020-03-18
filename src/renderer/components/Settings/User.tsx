7a868092353ba12581245dd967c01d96ed167abc
Gilles Coolen <gilleswhc@gmail.com>
2020-03-18 21:22:48 +0100

    Add user logout option

--- src/renderer/components/Settings/User.tsx
+++ src/renderer/components/Settings/User.tsx
@@ -2,9 +2,11 @@ import { hot } from 'react-hot-loader/root';
 import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
 import { AniList } from '../../../domain/anilist/anilist';
+import { motion } from 'framer-motion';
 
 const User = () => {
   const anilist = new AniList();
+  const [show, setShow] = useState(false);
   const [loggedIn, setLoggedIn] = useState(anilist.isLoggedIn());
 
   useEffect(() => {
@@ -16,33 +18,56 @@ const User = () => {
     setLoggedIn(true);
   }
 
-  const logout = () => {
+  const logout = async () => {
     anilist.logout();
     setLoggedIn(false);
+    setShow(false);
   }
+
+  const toggle = () => setShow(!show);
 
   return (
     <Container>
       {anilist.isLoggedIn()
-        ? <Profile onClick={logout} src={anilist.getCurrentUser()?.avatar.large} />
+        ? <Profile onClick={toggle} src={anilist.getCurrentUser()?.avatar.large} />
         : <Login onClick={login}>Login</Login>
+      }
+      {show &&
+        <Menu
+          initial={{ opacity: 0, x: 10 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          <div onClick={logout}>Logout</div>
+        </Menu>
       }
     </Container>
   )
 };
 
+const Menu = styled(motion.div)`
+  right: 0;
+  padding: 0.8rem;
+  font-size: 14px;
+  text-align: right;
+  position: absolute;
+  border-radius: 5px;
+  background-color: #202736;
+`;
+
 const Login = styled.button`
   color: white;
   border: none;
   font-weight: 700;
+  font-size: 14px;
   border-radius: 5px;
-  padding: 0.5rem 1rem;
+  padding: 0.6rem 0.8rem;
   transition: 0.2s all;
   background-color: #438c3c;
 
   &:hover {
-    transition: 0.1s all;
     cursor: pointer;
+    transition: 0.1s all;
     background-color: #4ead45;
   }
 `;
@@ -62,14 +87,9 @@ const Profile = styled.img`
   object-fit: cover;
   border-radius: 100%;
   transition: 0.2s all;
-  border: 4px solid white;
-  background-color: black;
+  border: 4px solid #202736;
+  background-color: #181b21;
   box-shadow: 0px 0px 8px 2px rgba(0,0,0,0.4);
-
-  &:hover {
-    transition: 0.1s all;
-    border-radius: 100% 100% 0 0;
-  }
 `;
 
 export default hot(User);
