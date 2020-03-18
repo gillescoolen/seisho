f286b55f8fb9e3a6bbef18a95cd2d02f28227773
Matthijs <matthijsk2000@gmail.com>
2020-03-18 09:31:29 +0100

    show spinner when syncing

--- src/renderer/components/Tracker/MangaTracker.tsx
+++ src/renderer/components/Tracker/MangaTracker.tsx
@@ -4,20 +4,26 @@ import { hot } from 'react-hot-loader/root';
 import { Manga } from '../../../domain/manga/manga';
 import { AniList } from '../../../domain/anilist/anilist';
 import { MediaListStatus } from '../../../domain/anilist/types';
+import Spinner from '../UI/Spinner';
 
 const MangaTracker = (props: { manga: Manga, tracker: AniList }) => {
   const [manga] = useState<Manga>(props.manga);
   const [tracked, setTracked] = useState(false);
+  const [syncing, setSyncing] = useState(false);
+
 
   useEffect(() => {
     (async () => {
+      setSyncing(true);
       manga.setTracker(props.tracker);
       await manga.syncFromTracker();
+      setSyncing(false);
     })();
     setTracked(manga.tracking());
   }, [tracked]);
 
   const add = async () => {
+    setSyncing(true);
     const result = await props.tracker.search(manga.getTitle(), 1);
     const counterpart = result.media[0];
 
@@ -27,17 +33,22 @@ const MangaTracker = (props: { manga: Manga, tracker: AniList }) => {
     await manga.syncToTracker();
 
     setTracked(true);
+    setSyncing(false);
   };
 
   const sync = async () => {
+    setSyncing(true);
     await manga.syncToTracker();
+    setSyncing(false);
   };
   return (
     <Container>
-      {tracked
-        ? <Button onClick={sync}>Sync to AniList</Button>
-        : <Button onClick={add}>Add to AniList</Button>
+      {syncing ? <Spinner/>
+        : (tracked
+          ? <Button onClick={sync}>Sync to AniList</Button>
+          : <Button onClick={add}>Add to AniList</Button>)
       }
+
     </Container>
   );
 };
